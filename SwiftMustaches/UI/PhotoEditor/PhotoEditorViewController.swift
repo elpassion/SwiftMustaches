45a98fada43fbf2ab1a5327baf7be09ee9d643f1
Dariusz Rybicki <darrarski@gmail.com>
2014-09-18 23:01:58 +0200

    PhotoEditorViewController and Storyboard

---
+++ SwiftMustaches/UI/PhotoEditor/PhotoEditorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoEditorViewController.swift
+//  SwiftMustaches
+//
+//  Created by Dariusz Rybicki on 18/09/14.
+//  Copyright (c) 2014 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+class PhotoEditorViewController: UIViewController {
+    
+    
+    
+}
