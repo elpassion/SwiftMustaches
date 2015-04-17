dfbd946b0b9b742f3bbb919f0bb222a3056c58b0
Dariusz Rybicki <darrarski@gmail.com>
2015-04-17 18:18:46 +0200

    Fastlane tools integration

---
+++ .fastlane/actions/build_signed_ipa.rb
@@ -0,0 +1,89 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      IPA_OUTPUT_PATH = :IPA_OUTPUT_PATH
+    end
+
+    class BuildSignedIpaAction < Action
+      def self.run(params)
+        params = params.first
+        
+        project_path          = params[:project_path]
+        workspace_path        = params[:workspace_path]
+        scheme                = params[:scheme]
+        configuration         = params[:configuration]
+        sdk                   = params[:sdk]
+        build_path            = params[:build_path]
+        app_name              = params[:app_name]
+        sign_identity         = params[:sign_identity]
+        mobileprovision_path  = params[:mobileprovision_path]
+
+        Helper.log.info 'Building application...'
+
+        unless workspace_path.nil?
+          path_param = "-workspace \"#{workspace_path}\""
+        else
+          path_param = "-project \"#{project_path}\""
+        end
+
+        Actions.sh [
+          "xctool",
+          "clean",
+          "build",
+          path_param,
+          "-scheme", "\"#{scheme}\"",
+          "-configuration", "\"#{configuration}\"",
+          "-sdk", "\"#{sdk}\"",
+          "ONLY_ACTIVE_ARCH=NO",
+          "CONFIGURATION_BUILD_DIR=\"#{build_path}\""
+        ].join(" ")
+        
+        Helper.log.info 'Creating signed IPA file...'
+
+        ipa_path = "#{build_path}/#{app_name}.ipa"
+
+        Actions.sh [
+          "xcrun",
+          "-log", 
+          "-sdk", "#{sdk}", 
+          "PackageApplication" ,
+          "\"#{build_path}/#{app_name}.app\"", 
+          "-o", "\"#{ipa_path}\"", 
+          "-sign", "\"#{sign_identity}\"", 
+          "-embed", "\"#{mobileprovision_path}\""
+        ].join(" ")
+
+        Actions.lane_context[Actions::SharedValues::IPA_OUTPUT_PATH] = "#{ipa_path}"
+
+      end
+
+      def self.description
+        "Clean and build using xctool then create IPA with xcrun PackageApplication"
+      end
+
+      def self.available_options
+        [
+          ['project_path', 'Path to .xcodeproj file'],
+          ['workspace_path', 'Path to .xcworkspace file'],
+          ['scheme', 'Build scheme'],
+          ['configuration', 'Build configuration (e.g. "Release")'],
+          ['sdk', 'SDK to use (e.g. "iphoneos")'],
+          ['build_path', 'Build directory path'],
+          ['app_name', 'Application name'],
+          ['sign_identity', 'Signing identity (e.g. "iPhone Distribution: MyCompany")'],
+          ['mobileprovision_path', 'Path to .mobileprovision file']
+        ]
+      end
+
+      def self.output
+        [
+          ['IPA_OUTPUT_PATH', 'Path to IPA file']
+        ]
+      end
+
+      def self.author
+        "Darrarski"
+      end
+    end
+  end
+end
